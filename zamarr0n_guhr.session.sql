870931032cfed2f99c5215576c6dc12fddb0fe9c
Emilio Zamarron <emilio.zamarrons@gmail.com>
2024-07-08 22:43:27 -0600

    .

--- zamarr0n_guhr.session.sql
+++ zamarr0n_guhr.session.sql
@@ -1,9 +1,9 @@
 -- INSERT INTO inventory (inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color, classification_id) 
 -- VALUES ('make', 'model', '1000', 'descrp', 'img', 'thumbnail', '10909', 1000,'black', 7); 
 
--- SELECT * FROM inventory;
+SELECT * FROM inventory;
 
--- DELETE FROM INVENTORY WHERE inv_id = 48;
+-- DELETE FROM INVENTORY WHERE inv_id = 50;
 -- SELECT * FROM inventory;
 -- custom - 1
 -- sport - 2 
