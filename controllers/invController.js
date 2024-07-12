696b3ddb488620c18d9fe1ca0f668476e59d89ac
Emilio Zamarron <emilio.zamarrons@gmail.com>
2024-07-11 22:11:17 -0600

    fixing bugs - misspellings inside my code.

--- controllers/invController.js
+++ controllers/invController.js
@@ -12,7 +12,10 @@ const classification_id = req.params.classificationId
 const data = await invModel.getInventoryByClassificationId(classification_id)
 const grid = await utilities.buildClassificationGrid(data)
 let nav = await utilities.getNav()
-const className = data[0].classification_name
+let className = "Unknown";
+if (data && data.length) {
+className = data[0].classification_name
+}
 res.render("./inventory/classification", {
     title: className + " vehicles",
     nav,
